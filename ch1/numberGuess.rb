16279d0bc4a70443968aec307ee85dcb08c1350f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-25 21:25:18 +0100

    Loop through input check while user has not guessed correctly

--- ch1/numberGuess.rb
+++ ch1/numberGuess.rb
@@ -9,21 +9,25 @@ puts("Welcome, #{name}!")
 target = rand(100) + 1
 puts("I have picked a random number between 1 and 100, can you guess it?")
 num_guesses = 0
-remaining_guesses = 10 - num_guesses
-puts("You have #{remaining_guesses.to_s} guesses remaining")
-
-guess = gets.to_i
 guessed_correct = false
 
-if guess < target
-    puts("Your guess was too low! Try again!")
-    num_guesses += 1
-elsif guess > target
-    puts("Your guess was too high! Try again!")
-    num_guesses += 1
-else
-    puts("Your guess was correct! Well done #{name}!")
-    guessed_correct = true
+while num_guesses < 10 && guessed_correct == false
+    remaining_guesses = 10 - num_guesses
+    puts("You have #{remaining_guesses.to_s} guesses remaining")
+
+    guess = gets.to_i
+
+    if guess < target
+        puts("Your guess was too low! Try again!")
+        num_guesses += 1
+    elsif guess > target
+        puts("Your guess was too high! Try again!")
+        num_guesses += 1
+    else
+        puts("Your guess was correct! Well done #{name}!")
+        puts("You guessed the number with #{num_guesses} attempts!")
+        guessed_correct = true
+    end
 end
 
 if guessed_correct == false
