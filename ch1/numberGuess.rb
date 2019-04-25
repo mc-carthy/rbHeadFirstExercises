71d9a3a36a6a5962fb6d9b2e759f163207447fab
Mick McCarthy <mccarthy.mick0012@gmail.com>
2019-04-25 21:19:14 +0100

    Add guess inputting and checking

--- ch1/numberGuess.rb
+++ ch1/numberGuess.rb
@@ -4,3 +4,28 @@ print('Please enter your name: ')
 input = gets
 name = input.chomp
 puts("Welcome, #{name}!")
+
+# Add 1 as rand(x) will give a number between 0 and x - 1
+target = rand(100) + 1
+puts("I have picked a random number between 1 and 100, can you guess it?")
+num_guesses = 0
+remaining_guesses = 10 - num_guesses
+puts("You have #{remaining_guesses.to_s} guesses remaining")
+
+guess = gets.to_i
+guessed_correct = false
+
+if guess < target
+    puts("Your guess was too low! Try again!")
+    num_guesses += 1
+elsif guess > target
+    puts("Your guess was too high! Try again!")
+    num_guesses += 1
+else
+    puts("Your guess was correct! Well done #{name}!")
+    guessed_correct = true
+end
+
+if guessed_correct == false
+    puts("Sorry! The correct number was #{target.to_s}")
+end
